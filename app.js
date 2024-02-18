4d2e0546030a376385d82ad54bb7a7ba77f7e1b5
Lane Coker <106985382+laneonguitar@users.noreply.github.com>
2024-02-17 21:38:23 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+ 
+
+// For Loop
+
+   // Donut array
+const donuts = ["Regular Glazed", "Chocolate Glazed", "Maple Glazed", "Strawberry Glazed", "Chocolate Glazed with Sprinkles"]; 
+
+// Loop list out each donut
+for (let i = 0; i < donuts.length; i++) {
+  console.log("Donuts " + (i + 1) + ": " + donuts[i]);
+}
+
+console.log("Boy am I HUNGRY!!");
+
+  
+
+
+
+
+
